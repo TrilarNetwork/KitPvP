6c8299a0becb77dbb043e3408cce10f66d348f2b
Samie <Samie@iMelvin>
2018-01-07 21:51:42 +0100

    fixes

--- Trilar_KitPvpCore/src/me/imelvin/kitpvp/utils/KitPvPGUI.java
+++ Trilar_KitPvpCore/src/me/imelvin/kitpvp/utils/KitPvPGUI.java
@@ -3,6 +3,7 @@ package me.imelvin.kitpvp.utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -11,51 +12,59 @@ import org.bukkit.potion.PotionType;
 
 import me.imelvin.kitpvp.utils.Kits.Kit;
 import me.shizleshizle.core.commands.Skulls;
+import me.shizleshizle.core.objects.User;
 import me.shizleshizle.core.utils.CI;
 import me.shizleshizle.core.utils.T_ItemStack;
 
 public class KitPvPGUI {
-	
+
 	public static void globalMenu(Player p) {
-		Inventory inv = Bukkit.createInventory(null, 3*9, ChatColor.GOLD + "KitPvP Menu");
-		
+		Inventory inv = Bukkit.createInventory(null, 3 * 9, ChatColor.GOLD + "KitPvP Menu");
+
 		ItemStack profile = Skulls.getSkull(p.getName(), 1, ChatColor.GOLD + p.getName() + "'s Profile");
 		ItemStack join = CI.createItem(Material.DIAMOND_SWORD, 1, -1, ChatColor.DARK_BLUE + "Join the game");
 		ItemStack kits = CI.createItem(Material.CHEST, 1, -1, ChatColor.DARK_AQUA + "Kits");
-		
+
 		inv.setItem(11, profile);
 		inv.setItem(13, join);
 		inv.setItem(15, kits);
 		p.openInventory(inv);
 	}
-	
+
 	public static void profileMenu(Player p) {
-		Inventory inv = Bukkit.createInventory(null, 5*9, ChatColor.GOLD + "Profile");
-		
-		ItemStack kills = CI.createItem(Material.DIAMOND_SWORD, 1, -1, ChatColor.GOLD + "Kills: " + ChatColor.YELLOW + PlayerDataManager.getKills(p)); // add kills
-		ItemStack deaths = CI.createItem(Material.SKULL_ITEM, 1, 0, ChatColor.DARK_RED + "Deaths: " + ChatColor.YELLOW + PlayerDataManager.getDeaths(p)); // add deaths;
-		ItemStack ratio = CI.createItem(Material.DEAD_BUSH, 1, -1, ChatColor.DARK_BLUE + "K/D Ratio: " + ChatColor.YELLOW + getKDRatio(p)); // add ratio
-		ItemStack lstreak = CI.createItem(Material.DRAGON_EGG, 1, -1, ChatColor.DARK_GREEN + "Longest Streak: " + ChatColor.YELLOW + PlayerDataManager.getStreak(p)); // streak
+		Inventory inv = Bukkit.createInventory(null, 5 * 9, ChatColor.GOLD + "Profile");
+
+		ItemStack kills = CI.createItem(Material.DIAMOND_SWORD, 1, -1,
+				ChatColor.GOLD + "Kills: " + ChatColor.YELLOW + PlayerDataManager.getKills(p)); // add
+																								// kills
+		ItemStack deaths = CI.createItem(Material.SKULL_ITEM, 1, 0,
+				ChatColor.DARK_RED + "Deaths: " + ChatColor.YELLOW + PlayerDataManager.getDeaths(p)); // add
+																										// deaths;
+		ItemStack ratio = CI.createItem(Material.DEAD_BUSH, 1, -1,
+				ChatColor.DARK_BLUE + "K/D Ratio: " + ChatColor.YELLOW + getKDRatio(p)); // add
+																							// ratio
+		ItemStack lstreak = CI.createItem(Material.DRAGON_EGG, 1, -1,
+				ChatColor.DARK_GREEN + "Longest Streak: " + ChatColor.YELLOW + PlayerDataManager.getStreak(p)); // streak
 		ItemStack back = CI.createItem(Material.DARK_OAK_DOOR, 1, -1, ChatColor.RED + "Back to KitPvP Menu");
-		
+
 		inv.setItem(11, kills);
 		inv.setItem(13, deaths);
 		inv.setItem(15, ratio);
 		inv.setItem(30, lstreak);
 		inv.setItem(32, back);
-		p.openInventory(inv);	
+		p.openInventory(inv);
 	}
-	
-	//FOR ABOVE
+
+	// FOR ABOVE
 	private static double getKDRatio(Player p) {
 		int kills = PlayerDataManager.getKills(p);
 		int deaths = PlayerDataManager.getDeaths(p);
 		double fin = kills / deaths;
 		return fin;
 	}
-	
+
 	public static void kitsMenu(Player p) {
-		Inventory inv = Bukkit.createInventory(null, 4*9, ChatColor.GOLD + "Kits");
+		Inventory inv = Bukkit.createInventory(null, 4 * 9, ChatColor.GOLD + "Kits");
 		Kit k = null;
 		if (Kits.kits.containsKey(p.getName())) {
 			k = Kits.kits.get(p.getName());
@@ -103,12 +112,13 @@ public class KitPvPGUI {
 		Potion strengthpot = new Potion(PotionType.STRENGTH, 1);
 		strengthpot.setSplash(true);
 		ItemStack strengthpotion = strengthpot.toItemStack(1);
-		invisinvp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Strength Kit"); 
-		
+		invisinvp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Strength Kit");
+
 		ItemStack i = CI.createItem(Material.DARK_OAK_DOOR, 1, -1, ChatColor.DARK_AQUA + "Go back to main menu!");
-		ItemStack i2 = CI.createItem(Material.SIGN_POST, 1, -1, ChatColor.WHITE + "Information", ChatColor.GOLD + "Right-Click an item to select the kit.", 
+		ItemStack i2 = CI.createItem(Material.SIGN_POST, 1, -1, ChatColor.WHITE + "Information",
+				ChatColor.GOLD + "Right-Click an item to select the kit.",
 				ChatColor.YELLOW + "Left-Click an item to preview the kit.");
-		
+
 		inv.setItem(2, k1.toItemStack());
 		inv.setItem(4, k2.toItemStack());
 		inv.setItem(6, k3.toItemStack());
@@ -123,5 +133,101 @@ public class KitPvPGUI {
 		inv.setItem(31, i);
 		inv.setItem(35, i2);
 		p.openInventory(inv);
+	}
+
+	public static void openTank(User p) {
+		Inventory inv = Bukkit.createInventory(null, 6 * 9, ChatColor.GOLD + "Tank kit");
+
+		ItemStack tankhelmet = CI.createItem(Material.CHAINMAIL_HELMET, 1, -1, ChatColor.DARK_AQUA + "Tank Helmet");
+		tankhelmet.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack tankchestplate = CI.createItem(Material.CHAINMAIL_CHESTPLATE, 1, -1,
+				ChatColor.DARK_AQUA + "Tank Chestplate");
+		tankchestplate.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack tankleggings = CI.createItem(Material.CHAINMAIL_LEGGINGS, 1, -1,
+				ChatColor.DARK_AQUA + "Tank Leggings");
+		tankleggings.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack tankboots = CI.createItem(Material.CHAINMAIL_BOOTS, 1, -1, ChatColor.DARK_AQUA + "Tank Boots");
+		tankboots.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack tanksword = CI.createItem(Material.STONE_SWORD, 1, -1, ChatColor.DARK_AQUA + "Tank Sword");
+		ItemStack tankbow = CI.createItem(Material.BOW, 1, -1, ChatColor.DARK_AQUA + "Tank Bow");
+		tankbow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 1);
+		tankbow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+		ItemStack tankarrow = CI.createItem(Material.ARROW, 1, -1, ChatColor.DARK_AQUA + "Tank Arrow");
+		ItemStack tankgapple = CI.createItem(Material.GOLDEN_APPLE, 3, 0, ChatColor.DARK_AQUA + "Tank Apple");
+		ItemStack tankpearl = CI.createItem(Material.ENDER_PEARL, 4, -1, ChatColor.DARK_AQUA + "Tank Pearl");
+		Potion tankspeed = new Potion(PotionType.SPEED, 1);
+		tankspeed.setSplash(true);
+		ItemStack tankspeedp = tankspeed.toItemStack(1);
+		tankspeedp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Tank Speed Pot");
+		Potion tankheal = new Potion(PotionType.INSTANT_HEAL, 1);
+		tankheal.setSplash(true);
+		ItemStack tankhealp = tankheal.toItemStack(1);
+		tankhealp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Tank Heal Pot");
+		Potion tankregen = new Potion(PotionType.REGEN, 1);
+		tankregen.setSplash(true);
+		ItemStack tankregp = tankregen.toItemStack(1);
+		tankregp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Tank Regen Pot");
+
+		inv.setItem(20, tankhelmet);
+		inv.setItem(21, tankchestplate);
+		inv.setItem(22, tankleggings);
+		inv.setItem(23, tankboots);
+		inv.setItem(19, tanksword);
+		inv.setItem(24, tankbow);
+		inv.setItem(25, tankarrow);
+		inv.setItem(29, tankgapple);
+		inv.setItem(33, tankpearl);
+		// inv.setItem(30, tankheal);
+		// inv.setItem(31, tankspeed);
+		// inv.setItem(32, tankregen);
+	}
+
+	public static void openPvp(User p) {
+		Inventory inv = Bukkit.createInventory(null, 6 * 9, ChatColor.GOLD + "Pvp Kit");
+
+		ItemStack pvphelmet = CI.createItem(Material.LEATHER_HELMET, 1, -1, ChatColor.GOLD + "PvP Helmet");
+		pvphelmet.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack pvpchestplate = CI.createItem(Material.LEATHER_CHESTPLATE, 1, -1, ChatColor.GOLD + "PvP Chestplate");
+		pvpchestplate.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack pvpleggings = CI.createItem(Material.LEATHER_LEGGINGS, 1, -1, ChatColor.GOLD + "PvP Leggings");
+		pvpleggings.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack pvpboots = CI.createItem(Material.LEATHER_BOOTS, 1, -1, ChatColor.GOLD + "PvP Boots");
+		pvpboots.addEnchantment(Enchantment.DURABILITY, 3);
+		ItemStack pvpsword = CI.createItem(Material.STONE_SWORD, 1, -1, ChatColor.GOLD + "PvP Sword");
+		ItemStack pvpbow = CI.createItem(Material.BOW, 1, -1, ChatColor.GOLD + "PvP Bow");
+		pvpbow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 1);
+		pvpbow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+		ItemStack pvparrow = CI.createItem(Material.ARROW, 1, -1, ChatColor.GOLD + "PvP Arrow");
+		ItemStack pvpgapple = CI.createItem(Material.GOLDEN_APPLE, 3, 0, ChatColor.GOLD + "PvP Apple");
+		ItemStack pvppearl = CI.createItem(Material.ENDER_PEARL, 4, -1, ChatColor.GOLD + "PvP Pearl");
+		Potion pvpspeed = new Potion(PotionType.SPEED, 1);
+		pvpspeed.setSplash(true);
+		ItemStack pvpspeedp = pvpspeed.toItemStack(1);
+		pvpspeedp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Pvp Speed Pot");
+		Potion pvpheal = new Potion(PotionType.INSTANT_HEAL, 1);
+		pvpheal.setSplash(true);
+		ItemStack pvphealp = pvpheal.toItemStack(1);
+		pvphealp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Pvp Heal Pot");
+		Potion pvpregen = new Potion(PotionType.REGEN, 1);
+		pvpregen.setSplash(true);
+		ItemStack pvpregp = pvpregen.toItemStack(1);
+		pvpregp.getItemMeta().setDisplayName(ChatColor.DARK_AQUA + "Pvp Regen Pot");
+
+		inv.setItem(20, pvphelmet);
+		inv.setItem(21, pvpchestplate);
+		inv.setItem(22, pvpleggings);
+		inv.setItem(23, pvpboots);
+		inv.setItem(19, pvpsword);
+		inv.setItem(24, pvpbow);
+		inv.setItem(25, pvparrow);
+		inv.setItem(29, pvpgapple);
+		inv.setItem(33, pvppearl);
+		// inv.setItem(30, pvpheal);
+		// inv.setItem(31, pvpspeed);
+		// inv.setItem(32, pvpregen);
+	}
+
+	public static void openNoDebuff(User p) {
+		Inventory inv = Bukkit.createInventory(null, 6 * 9, ChatColor.GOLD + "No debuff Kit");
 	}
 }
